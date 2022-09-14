b5078a50c4bc2984dc606aad4b58f303c8b304e8
FilipHrn <filip.fwb@hotmail.com>
2022-09-14 21:19:07 +0200

    Created Header component

---
+++ components/Header/partials/Navigation.jsx
@@ -0,0 +1,14 @@
+
+function Navigation() {
+
+  return (
+    <nav>
+      <ul className="flex space-x-3">
+        <li><a className="text-gray-400" href="/">Home</a></li>
+        <li><a className="text-gray-400" href="/login">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
