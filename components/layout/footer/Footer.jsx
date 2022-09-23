58b187c940f7bd28555030c0012ca40c405ce715
FilipHrn <filip.fwb@hotmail.com>
2022-09-23 02:31:31 +0200

    Added a footer

---
+++ components/layout/footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer className="p-3 text-center shadow-[0_0px_3px_1px_rgba(0,0,0,0.1)] text-sm bg-white">
+      <p>Made by Filip Hrnjez</p>
+    </footer>
+  )
+}
+
+export default Footer
